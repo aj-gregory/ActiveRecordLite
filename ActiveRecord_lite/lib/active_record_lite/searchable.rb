f626722f23069fcc62b4d5e16b0ab107c6eb5d2b
AJ Gregory <bertie458@gmail.com>
2013-10-07 16:13:51 -0700

    implement searchable module

--- ActiveRecord_lite/lib/active_record_lite/searchable.rb
+++ ActiveRecord_lite/lib/active_record_lite/searchable.rb
@@ -2,5 +2,17 @@ require_relative './db_connection'
 
 module Searchable
   def where(params)
+  	mapped_keys = params.keys.map { |key| "#{key} = ?" }
+  	vals = params.values
+
+  	hash_array =DBConnection.execute(<<-SQL, *vals)
+  	  SELECT *
+  	  FROM "#{self.table_name}"
+  	  WHERE #{mapped_keys.join(', ')}
+  	SQL
+
+    object_array = []
+  	hash_array.each { |hash| object_array << self.new(hash)}
+    object_array
   end
 end
