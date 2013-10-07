cc1cd9ad34537b249eef36db93cf85a67d3e18c8
AJ Gregory <bertie458@gmail.com>
2013-10-07 14:26:06 -0700

    sql_object methods find, all, and create functional

--- ActiveRecord_lite/lib/active_record_lite/sql_object.rb
+++ ActiveRecord_lite/lib/active_record_lite/sql_object.rb
@@ -5,26 +5,73 @@ require_relative './searchable'
 
 class SQLObject < MassObject
   def self.set_table_name(table_name)
+    @table_name = table_name
   end
 
   def self.table_name
+    @table_name
   end
 
   def self.all
+    DBConnection.execute(<<-SQL)
+      SElECT *
+      FROM "#{self.table_name}"
+    SQL
   end
 
   def self.find(id)
+    DBConnection.execute(<<-SQL, id)
+      SElECT *
+      FROM "#{self.table_name}"
+      WHERE ? = id 
+    SQL
   end
 
   def create
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO [#{self.class.table_name}] (#{self.class.attributes.join(',')})
+      VALUES (#{(['?'] * 3).join(',')})
+    SQL
   end
 
   def update
+    DBConnection.execute(<<-SQL, *values)
+      UPDATE [#{self.table_name}]
+      SET 
+    SQL
+    self.id = DBConnection.last_insert_row_id
   end
 
   def save
   end
 
   def attribute_values
+    self.class.attributes.map { |attribute| self.send(attribute) }
   end
 end
+
+#--TESTS--#
+
+cats_db_file_name =
+  File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
+DBConnection.open(cats_db_file_name)
+
+class Cat < SQLObject
+  set_table_name("cats")
+  my_attr_accessible(:id, :name, :owner_id)
+end
+
+class Human < SQLObject
+  set_table_name("humans")
+  my_attr_accessible(:id, :fname, :lname, :house_id)
+end
+
+p Human.superclass
+p Human.find(1)
+p Cat.find(1)
+p Cat.find(2)
+
+p Human.all
+p Cat.all
+
+c = Cat.new(:name => "Gizmo", :owner_id => 1)
