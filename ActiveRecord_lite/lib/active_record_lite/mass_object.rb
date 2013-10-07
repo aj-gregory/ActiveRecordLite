495604c6e93959c093e00663ffbf48f5ffd8015d
AJ Gregory <bertie458@gmail.com>
2013-10-07 12:52:47 -0700

    Completed MassObject

--- ActiveRecord_lite/lib/active_record_lite/mass_object.rb
+++ ActiveRecord_lite/lib/active_record_lite/mass_object.rb
@@ -1,13 +1,26 @@
 class MassObject
   def self.my_attr_accessible(*attributes)
+  	attributes.each do |attribute|
+  	  self.attributes << attribute
+  	  self.send(:attr_accessor, attribute)
+  	end
   end
 
   def self.attributes
+  	@attributes ||= []
   end
 
   def self.parse_all(results)
   end
 
   def initialize(params = {})
+  	params.keys.each do |param|
+  	  if self.class.attributes.include?(param)
+  	  	self.send("#{param}=", params[param])
+  	  else
+  	    raise "mass assignment to unregistered attribute #{param}"
+  	 end
+    end
   end
+
 end
